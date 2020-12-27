0229b0896b2080f562a0ad34e21cc3b747302510
Gevin Delta <gdelta@am-l1.msk.21-school.ru>
2020-12-27 18:50:13 +0300

    it works

--- get_next_line.c
+++ get_next_line.c
@@ -17,23 +17,28 @@ int next_line_exists(char **next_line, char **line)
     char *line_break;
 
 	line_break = NULL;
-	*line = ft_strdup(*next_line);
-    if ((line_break = ft_strchr(*line, '\n')))
+    if ((line_break = ft_strchr(*next_line, '\n')))
     {
-        if (line_break++)
-		{
-            if (*line_break != '\n')
-                --line_break;
+        //if (line_break + 1)
+		//{
             *line_break = '\0';
-            ft_strcpy(*next_line, ++line_break);
-        }
+            *line = ft_strdup(*next_line);
+            if (*(line_break + 1))
+                ft_strcpy(*next_line, ++line_break);
+            else
+            {
+                free(*next_line);
+		        *next_line = NULL;
+                return (0);
+            }
 		return (1);
     }
     else
 	{
+        *line = ft_strdup(*next_line);
 		free(*next_line);
 		*next_line = NULL;
-		return (0);
+		return (1);
 	}
 }
 
@@ -45,44 +50,59 @@ int get_next_line(int fd, char **line)
     static char *next_line;
     char *tmp;
 
-    if (fd < 1 || !line || BUFFER_SIZE < 1)
+    if (fd < 0 || !line || BUFFER_SIZE < 1)
         return (-1);
     line_break = NULL;
+    byte_reader = 1;
     if (next_line)
-        next_line_exists(&next_line, line);
+    {
+        if (!(next_line_exists(&next_line, line)))
+            return (1);
+    }
     else
         *line = (char*)ft_calloc(1, 1);
-    while(!next_line && !line_break && (byte_reader = read(fd, buf, BUFFER_SIZE)))
+    while(!next_line && !line_break && byte_reader)
     {
+        if ((byte_reader = read(fd, buf, BUFFER_SIZE)) < 0)
+            return (-1);
         buf[byte_reader] = '\0';
         if((line_break = ft_strchr(buf, '\n')))
         {
             *line_break = '\0';
-            next_line = ft_strdup(++line_break);
+            if (*(line_break + 1))
+                next_line = ft_strdup(++line_break);
+            else
+            {
+                free(next_line);
+		        next_line = NULL;
+	        }
         }
         tmp = *line;
         *line = ft_strjoin(*line, buf);
         free(tmp);
     }
-    if (byte_reader || ft_strlen(next_line) || ft_strlen(*line))
-        return (1);
-	return(0);
-}
-
-int main ()
-{
-    char *line;
-    int fd;
-    int a;
-
-   fd = open("../text.txt", O_RDONLY);
-    while ((a = get_next_line(fd, &line)))
-    {
-        //printf("%d\n\n", a);
-		printf("%s\n", line);
-		free(line);
-    }
-    free(line);
-   	//sleep(100);
+    if (byte_reader || ft_strlen(next_line))
+       return (1);
+	//return ((byte_reader == 0) ? 0 : 1);
     return (0);
 }
+
+// int main ()
+// {
+//     char *line;
+//     int fd;
+//     int a;
+
+//     a = 1;
+//    fd = open("text.txt", O_RDONLY);
+//     while (a)
+//     {
+//         (a = get_next_line(fd, &line));
+//         //printf("%d\n\n", a);
+// 		printf("%s\n", line);
+// 		free(line);
+//     }
+//      //free(line);
+//    	// sleep(100);
+//     return (0);
+//
